8715c93df2110d4a9f84f2abcea17caa6f0a9fdf
Susannah Blundell <susannahblundell98@gmail.com>
2025-06-22 17:44:46 +0100

    Modified hyperparameter search space values.

--- model_building.py
+++ model_building.py
@@ -82,7 +82,7 @@ def count_meta(dataset, name, metadata_features, drop):
     if drop: # Drop the metadata if bool is true
         dataset = dataset.iloc[:, col_number:]
         col_number = 0 # Now removed all metadata so count is 0
-        print("Metadata was dropped from the dataset; if unintended, disable drop_metadata in the script.")
+        print(f"Metadata was dropped from {name}; if unintended, disable drop_metadata in the script.")
     return col_number,dataset
 
 # Call function
@@ -249,10 +249,10 @@ def basic_train(model, X_train, y_train, identifier, scores_dict):
     print(f"Finished training {identifier}")
 
 # Determine which models to test
-logistic_regression = False
+Logistic_regression = True
 SVM = False
 Random_forest = False
-AdaBoost = True
+AdaBoost = False
 Gradient_boosting = False
 XGBoost = False
 KNN = False
@@ -272,8 +272,8 @@ best_models_fs = {}
 # Outline each model and perform the basic training function to evaluate performance of each
 if basic_training:
     # Logistic Regression
-    if logistic_regression:
-        log_reg = LogisticRegression(solver='saga', tol=1e-3, max_iter=200) # Reduced tolerance vs default so coef_ can converge
+    if Logistic_regression:
+        log_reg = LogisticRegression(solver='saga', tol=1e-4, max_iter=1500)
         basic_train(log_reg, X_train, y_train, 'Logistic Regression', top_model_scores)
 
     # SVM
@@ -391,12 +391,12 @@ def objective(params):
         params['min_samples_split'] = int(params['min_samples_split'])
         params['min_samples_leaf'] = int(params['min_samples_leaf'])
         clf = GradientBoostingClassifier(**params)
-    elif classifier_type == 'ada': #TODO ada and knn were recently added so not tested - e.g. which params need integer conversion as above
-        max_depth = int(params.pop('max_depth')) # max_depth is a parameter of the estimator param (model) so remove from params dict/search space and use here instead
+    elif classifier_type == 'ada':
         params['n_estimators'] = int(params['n_estimators'])
-        params['estimator'] = DecisionTreeClassifier(max_depth=max_depth) # Create the base estimator param to explore using max_depth
         clf = AdaBoostClassifier(**params)
     elif  classifier_type == 'knn':
+        params['n_neighbors'] = int(params['n_neighbors'])
+        params['leaf_size'] = int(params['leaf_size'])
         clf = KNeighborsClassifier(**params)
     else:
         return {'loss': 1, 'status': STATUS_OK}
@@ -427,66 +427,81 @@ best_spaces = [] # Initialise list
 if type_translation['svm'] in best_models_fs:
     best_spaces.append({
         'type': 'svm',
-        'C': hp.lognormal('SVM_C', 0, 1.0),
-        'kernel': hp.choice('svm_kernel', ['linear', 'rbf'])
+        'C': hp.lognormal('svm_C', 0, 1.0),
+        'kernel': hp.choice('svm_kernel', ['linear', 'rbf']),
+        'gamma': hp.choice('svm_gamma', ['scale', 'auto']),
+        'class_weight': hp.choice('svm_class_weight', [None, 'balanced']),
+        'random_state': 42,
     })
 # Random forest
 if type_translation['rf'] in best_models_fs:
     best_spaces.append({
         'type': 'rf',
-        'criterion': hp.choice('rf_criterion', ['gini', 'entropy']),
+        'criterion': hp.choice('rf_criterion', ['gini', 'entropy', 'log_loss']),
         'n_estimators': hp.quniform('rf_n_estimators', 50, 500, 50),
         'max_depth': hp.quniform('rf_max_depth', 2, 10, 1),
         'min_samples_split': hp.quniform('rf_min_samples_split', 2, 20, 1),
         'min_samples_leaf': hp.quniform('rf_min_samples_leaf', 1, 10, 1),
         'max_features': hp.choice('rf_max_features', ['sqrt', 'log2', 0.8]),
-        'class_weight': hp.choice('rf_class_weight', [None, 'balanced'])
+        'class_weight': hp.choice('rf_class_weight', [None, 'balanced']),
+        'random_state': 42,
     })
 # Logistic regression
 if type_translation['logreg'] in best_models_fs:
-    best_spaces.append({
-        'type': 'logreg',
-        'C': hp.lognormal('lr_C', 0, 1.0),
-        # 'solver': hp.choice('lr_solver', ['liblinear', 'lbfgs'])
-        'solver': 'saga',  # Force solver for elasticnet in feature selection
-        'penalty': 'elasticnet',
-        'l1_ratio': hp.uniform('l1_ratio', 0, 1),  # Required for ElasticNet
-    })
+   best_spaces.append({
+       'type': 'logreg',
+       'C': hp.lognormal('lr_C', 0, 1.0),
+       'solver': hp.choice('lr_solver', ['liblinear', 'saga']),
+       'penalty': hp.choice('lr_penalty', ['l1', 'l2']), # Chosen to be compatible with liblinear and saga
+       'class_weight': hp.choice('lr_class_weight', [None, 'balanced']),
+       'random_state': 42,
+       'max_iter': 3000
+   })
 # XGBoost
 if type_translation['xgb'] in best_models_fs:
     best_spaces.append({
         'type': 'xgb',
         'max_depth': hp.quniform("xgb_max_depth", 3, 15, 1),
-        'gamma': hp.uniform('xgb_gamma', 0, 9),
-        'reg_alpha': hp.quniform('xgb_reg_alpha', 0, 10, 1),
-        'reg_lambda': hp.uniform('xgb_reg_lambda', 0, 10),
-        'colsample_bytree': hp.uniform('xgb_colsample_bytree', 0.6, 1),
-        'min_child_weight': hp.quniform('xgb_min_child_weight', 0, 12, 1),
+        'gamma': hp.uniform('xgb_gamma', 1, 9),
+        'reg_alpha': hp.quniform('xgb_reg_alpha', 10, 180, 1),
+        'reg_lambda': hp.uniform('xgb_reg_lambda', 0, 5),
+        'colsample_bytree': hp.uniform('xgb_colsample_bytree', 0.5, 1),
+        'min_child_weight': hp.quniform('xgb_min_child_weight', 0, 10, 1),
         'n_estimators': hp.quniform('xgb_n_estimators', 100, 500, 50),
         'seed': 0,
         'learning_rate': hp.uniform('xgb_learning_rate', 0.01, 0.3),
-        'scale_pos_weight': hp.uniform('xgb_scale_pos_weight', 1, 10)  # Adjust if classes are imbalanced
+        'scale_pos_weight': hp.uniform('xgb_scale_pos_weight', 1, 10),  # Adjust if classes are imbalanced
+        'max_delta_step': hp.uniform('xgb_max_delta_step', 0, 10),
+        'random_state': 42,
     })
 # Gradient Boosting
 if type_translation['gb'] in best_models_fs:
     best_spaces.append({
         'type': 'gb',
-        'n_estimators': hp.quniform('gb_n_estimators', 50, 500, 50),
+        'n_estimators': hp.quniform('gb_n_estimators', 50, 250, 50),
         'max_depth': hp.quniform('gb_max_depth', 3, 15, 1),
         'min_samples_split': hp.quniform('gb_min_samples_split', 2, 20, 1),
         'min_samples_leaf': hp.quniform('gb_min_samples_leaf', 1, 10, 1),
-        'learning_rate': hp.loguniform('gb_learning_rate', np.log(0.005), np.log(0.2)),
+        'learning_rate': hp.loguniform('gb_learning_rate', 0.05, 0.5),
         'subsample': hp.uniform('gb_subsample', 0.6, 1.0),
         'max_features': hp.choice('gb_max_features', ['sqrt', 'log2', 0.8]),
-        'loss': hp.choice('gb_loss', ['log_loss', 'exponential'])
+        'loss': hp.choice('gb_loss', ['log_loss', 'exponential']),
+        'criterion': hp.choice('gb_criterion', ['friedman_mse', 'squared_error']),
+        'random_state': 42,
+        # IMPROVE - more tunable features; see sklearn documentation
     })
 # AdaBoost # WARNING Ada and KNN are untested - search spaces of these were omitted the first time - not sure if due to a reason or just missed out
 if type_translation['ada'] in best_models_fs:
     best_spaces.append({
         'type': 'ada',
-        'n_estimators': hp.quniform('ada_n_estimators', 30, 1030, 50),
+        'n_estimators': hp.uniform('ada_n_estimators', 30, 1000),
         'learning_rate': hp.uniform('ada_learning_rate', 0.1, 1.0),
-        'max_depth': hp.quniform('ada_max_depth', 1, 10, 1)
+        'estimator': hp.choice('ada_base_estimator', [
+            DecisionTreeClassifier(random_state=42),
+            LinearSVC(random_state=42), # WARNING: LinearSVC is untested
+            LogisticRegression(random_state=42),
+        ]),
+        'random_state': 42,
     })
 # K-Nearest Neighbors
 if type_translation['knn'] in best_models_fs:
@@ -494,7 +509,10 @@ if type_translation['knn'] in best_models_fs:
         'type': 'knn',
         'n_neighbors': hp.quniform('knn_n_neighbors', 1, 50, 1),
         'weights': hp.choice('knn_weights', ['uniform', 'distance']),
-        'p': hp.choice('knn_p', [1, 2])
+        'leaf_size': hp.uniform('knn_leaf_size', 10, 60),
+        'p': hp.choice('knn_p', [1, 2]),
+        'metric': hp.choice('knn_metric', ['minkowski', 'euclidean', 'cityblock']),
+        'random_state': 42,
     })
 
 
@@ -558,13 +576,12 @@ with mlflow.start_run():  # TODO need to find examples of this being done - unsu
         best_params['min_samples_leaf'] = int(best_params['min_samples_leaf'])
         classifier = GradientBoostingClassifier(**best_params)
     elif classifier_type == 'ada': #TODO ada and knn were recently added so not tested - e.g. which params need integer conversion as abov
-        max_depth = int(best_params.pop('max_depth')) # Remove param from best params as it is a parameter of the 'estimator' and not AdaBoost itself
         best_params['n_estimators'] = int(best_params['n_estimators'])
-        best_params['estimator'] = DecisionTreeClassifier(max_depth=max_depth)
         classifier = AdaBoostClassifier(**best_params)
     elif  classifier_type == 'knn':
-        #todo put best params in
-        clf = KNeighborsClassifier(**best_params)
+        best_params['n_neighbors'] = int(best_params['n_neighbors'])
+        best_params['leaf_size'] = int(best_params['leaf_size'])
+        classifier = KNeighborsClassifier(**best_params)
 
     # Create the final pipeline with feature selection and classifier # TODO not sure if i need pipeline here since I dont feed into cross_val_score?
     final_pipeline = Pipeline([
@@ -574,7 +591,7 @@ with mlflow.start_run():  # TODO need to find examples of this being done - unsu
 
     # Train on full training data
     final_pipeline.fit(X_train, y_train)
-
+# WARNING: From here needs testing/improvement
     # Print the selected features
     try: # TODO this was made when RFECV was used for all, so likely no longer works
         selected = final_pipeline.named_steps['feature_selector']
